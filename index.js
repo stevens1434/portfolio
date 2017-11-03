2cb8a590eaa9b2df868889342fb998d6ed5dac60
stevens1434 <stevens1434@gmail.com>
2017-11-03 00:02:36 -0700

    set up outline of page, fonts, css, etc. Did some work on placing information in page --- TO DO: add projects, add links to social media, etc

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+// var partials = require('express-partials');
 var app = express();
 
+app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+app.use(express.static(__dirname + '/public'));
+// app.use(partials());
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('home');
 });
 
 var server = app.listen(process.env.PORT || 3000);

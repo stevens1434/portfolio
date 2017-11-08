8e2bb59c8039c7a8244140726eb0f9df20a22aea
stevens1434 <stevens1434@gmail.com>
2017-11-08 12:41:55 -0800

    fixed ABOUT ME section to center div, which contains the picture of me, when the screen is ssmaller than 992px, 570px, and 540px -- also changed colors of transparent divs so the text is easier to read

--- public/js/script.js
+++ public/js/script.js
@@ -17,107 +17,98 @@ console.log("script.js file")
 
 
 $(document).ready(function () {
-console.log(document);
+  // console.log(document);
+  /*====================================
+  SCROLLING SCRIPTS
+  ======================================*/
+  $('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 1200, 'easeInOutExpo');
+    event.preventDefault();
+  });
+
+  /*====================================
+  SLIDER SCRIPTS
+  ======================================*/
+  $('#carousel-slider').carousel({
+    interval: 2000 //TIME IN MILLI SECONDS
+  });
+
+  /*====================================
+  VAGAS SLIDESHOW SCRIPTS
+  ======================================*/
+  $.vegas('slideshow', {
+    backgrounds: [
+      { src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
+      { src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },
+    ]
+  })('overlay', {
+    /** SLIDESHOW OVERLAY IMAGE **/
+    src: 'assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
+  });
+
+  /*====================================
+  POPUP IMAGE SCRIPTS
+  ======================================*/
+  $('.fancybox-media').fancybox({
+    openEffect: 'elastic',
+    closeEffect: 'elastic',
+    helpers: {
+      title: {
+        type: 'inside'
+      }
+    }
+  });
+
+  /*====================================
+  FILTER FUNCTIONALITY SCRIPTS
+  ======================================*/
+  $(window).load(function () {
+    var $container = $('#work-div');
+    $container.isotope({
+      filter: '*',
+      animationOptions: {
+        duration: 750,
+        easing: 'linear',
+        queue: false
+      }
+    });
+    $('.caegories a').click(function () {
+      $('.caegories .active').removeClass('active');
+      $(this).addClass('active');
+      var selector = $(this).attr('data-filter');
+      $container.isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 750,
+          easing: 'linear',
+          queue: false
+        }
+      });
+      return false;
+    });
+  });
+
+  /*====================================
+        ADDITIONAL SCRIPTS
+  ====================================*/
+  $(".flipper").hover(function() {
+    console.log("flipper hover");
+    $(".back").show();
+    $(".front").hide();
+  });
+
+
+});
+
 /*====================================
-SCROLLING SCRIPTS
-======================================*/
+      OUTISDE $(DOCUMENT).READY()
+====================================*/
 
-$('.scroll-me a').bind('click', function (event) { //just pass scroll-me in design and start scrolling
-var $anchor = $(this);
-$('html, body').stop().animate({
-scrollTop: $($anchor.attr('href')).offset().top
-}, 1200, 'easeInOutExpo');
-event.preventDefault();
-});
-
-
-/*====================================
-SLIDER SCRIPTS
-======================================*/
-
-
-$('#carousel-slider').carousel({
-interval: 2000 //TIME IN MILLI SECONDS
-});
-
-
-/*====================================
-VAGAS SLIDESHOW SCRIPTS
-======================================*/
-$.vegas('slideshow', {
-backgrounds: [
-{ src: 'assets/img/1.jpg', fade: 1000, delay: 9000 },
-{ src: 'assets/img/2.jpg', fade: 1000, delay: 9000 },
-]
-})('overlay', {
-/** SLIDESHOW OVERLAY IMAGE **/
-src: 'assets/js/vegas/overlays/06.png' // THERE ARE TOTAL 01 TO 15 .png IMAGES AT THE PATH GIVEN, WHICH YOU CAN USE HERE
-});
-
-
-/*====================================
-POPUP IMAGE SCRIPTS
-======================================*/
-$('.fancybox-media').fancybox({
-openEffect: 'elastic',
-closeEffect: 'elastic',
-helpers: {
-title: {
-type: 'inside'
-}
-}
-});
-
-
-/*====================================
-FILTER FUNCTIONALITY SCRIPTS
-======================================*/
-$(window).load(function () {
-var $container = $('#work-div');
-$container.isotope({
-filter: '*',
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-$('.caegories a').click(function () {
-$('.caegories .active').removeClass('active');
-$(this).addClass('active');
-var selector = $(this).attr('data-filter');
-$container.isotope({
-filter: selector,
-animationOptions: {
-duration: 750,
-easing: 'linear',
-queue: false
-}
-});
-return false;
-});
-
-});
-
-
-
-/*====================================
-WRITE YOUR CUSTOM SCRIPTS BELOW
-======================================*/
-
-
-  setTimeout(function() {
-    console.log(window);
-    $(".mainDiv").show();
-    $('.space').hide();
-    $(".spinner").hide();
-  }, 3000);
-
-
-
-
-
-
-
-
-});
+setTimeout(function() {
+  $(".mainDiv").show();
+  $(".space").hide();
+  $(".loader").hide();
+}, 3500);
